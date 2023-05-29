b50880ce096563e693345e2c69811c909a23f916
Anderson Vulto <andersonvulto204@gmail.com>
2023-05-29 08:38:55 -0300

    Initial Commit

---
+++ PeachTV/PeachTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeachTVApp.swift
+//  PeachTV
+//
+//  Created by Anderson  Vulto on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PeachTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
